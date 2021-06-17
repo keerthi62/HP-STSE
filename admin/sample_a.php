f3d342af3cf6a9e93a5e24159b77b7266e591c46
Keerthi B18049 <68141360+keerthi62@users.noreply.github.com>
2021-06-17 13:11:15 +0530

    Add files via upload

---
+++ admin/sample_a.php
@@ -0,0 +1,16 @@
+
+<html>
+  
+<head>
+<script src="https://app.simplefileupload.com/buckets/d5cdf1d92e5fad70a788a913dedfd673.js"></script>
+</head>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data"> <!--on form submission it redirects to upload.php-->
+  Select file to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload" required> <!--upload pdf file or text file -->
+  <input type="submit" value="Upload FIle" name="submit"> <!-- submit file -->
+</form>
+
+</body>
+</html>
---
+++ admin/upload.php
@@ -0,0 +1,46 @@
+<?php
+$conn = new mysqli("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306","bupdy87dvohbr4iw","gtkjmuz9kqpdnzxg","ovlz54dtty1jll9i");
+extract($_POST);
+$target_file = basename($fileToUpload);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Allow certain file formats
+if($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg"
+|| $imageFileType == "gif" ) {
+  echo "Sorry, JPG, JPEG, PNG & GIF files are not allowed.";
+  echo " ";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+  echo " ";
+// if everything is ok, try to upload file
+} else {
+  $sql = "SELECT * FROM files";
+  $result = $conn->query($sql);
+  $check = false;
+  while ($row = $result -> fetch_row()) {
+    if(basename($row[0]) == $target_file)
+    {
+      $check = true;
+    }
+  }
+  if ($check)
+  {
+    echo "File name already exists";
+  }
+  else
+  {
+    $query="INSERT INTO files VALUES ('$fileToUpload')";
+    if ($conn->query($query) === TRUE) {
+      echo "The file ". htmlspecialchars($target_file). " has been uploaded.";
+      echo " ";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+      echo " ";
+    }
+  }
+}
+?>
