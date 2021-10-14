02a16db60371531823f29cd41d730aa41986b392
Keerthi <68141360+keerthi62@users.noreply.github.com>
2021-10-14 19:08:28 +0530

    Update registration-form.php

--- student/registration-form.php
+++ student/registration-form.php
@@ -1,5 +1,4 @@
 <?php
-//create connection.
 $conn = new mysqli("g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306","bupdy87dvohbr4iw","gtkjmuz9kqpdnzxg","ovlz54dtty1jll9i");
 extract($_POST); //imports post variables
 if(isset($save)) //it checks whether we clicked the submit button or not.
@@ -32,10 +31,8 @@ if ($conn->query($query) === TRUE) { //if details are inserted succesfully
 $conn->close(); //close connection.
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Registration Form</title>
 <style>
 input,textarea{width:200px}
@@ -66,7 +63,7 @@ label{
 
 <body>
 <div align="center" id="table">
-<form method="post" enctype="multipart/form-data">
+<form method="post">
 <table width="500" height="300" border="1"> 
 <?php  
   if(isset($msg)){ //it shows the error msg .
