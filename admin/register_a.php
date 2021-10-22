7a88be840878c6aa9e3eadbbd9f8791518cec0e6
Keerthi <68141360+keerthi62@users.noreply.github.com>
2021-10-22 12:24:54 +0530

    Update register_a.php

--- admin/register_a.php
+++ admin/register_a.php
@@ -31,10 +31,8 @@ if ($conn->query($query) === TRUE) { //if details are inserted succesfully
 $conn->close(); //close connection.
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Registration Form</title>
 <style>
 input,textarea{width:200px}
@@ -66,7 +64,7 @@ label{
 
 <body>
 <div align="center" id="table">
-<form method="post" enctype="multipart/form-data">
+<form method="post">
 <table width="500" height="300" border="1">
 <?php
   if(isset($msg)){
